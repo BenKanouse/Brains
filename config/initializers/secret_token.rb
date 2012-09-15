d5c0782d96386268a67b2fe904a19d9b0722599d
kanobt61@gmail.com <kanobt61@gmail.com>
2012-09-15 01:31:14 -0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brains::Application.config.secret_token = '5fa8b216fc9d77537537ca3c62f346a4dea697becfc871d691340daea9239fbc633bc50e74e492baf887c6d0bb018051e5fa2926a8a16e42b193754e4439e781'
